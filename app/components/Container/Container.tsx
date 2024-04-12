db1f8e4f98cf401a4a4f6bfc0c78fa34fe4e78c9
Andrew Galicia <andrewlgalicia@gmail.com>
2024-04-11 21:59:36 -0700

    began navbar

---
+++ app/components/Container/Container.tsx
@@ -0,0 +1,19 @@
+'use client';
+import React from 'react'
+
+export default function Container({children}) {
+    return (
+        <div
+            className='
+                max-w=[2520px]
+                mx-auto
+                xl:px-20
+                md:px-2
+                sm:px-2
+                px-4
+            '
+            >
+            {children}
+        </div>
+    )
+}

522640ccff8aaf34b63d7d8f8e517bacef49555c
Andrew Galicia <andrewlgalicia@gmail.com>
2024-06-04 16:14:21 -0700

    reservations

--- app/reservations/ReservationClient.tsx
+++ app/reservations/ReservationClient.tsx
@@ -12,13 +12,59 @@ import Container from "../components/Container/Container"
 import ListingCard from "../components/listings/ListingCard"
 
 
-export default function ReservationClient() {
+interface ReservationClientProps {
+    reservations: SafeReservation[];
+    currentUser?: SafeUser | null;
+}
+
+export default function ReservationClient({reservations, currentUser}: ReservationClientProps) {
+    
+    const router = useRouter();
+    const [deletingId, setDeletingId] = useState('');
+    const onCancel = useCallback((id:string) => {
+        setDeletingId(id);
+
+        axios.delete(`/api/reservations/${id}`)
+        .then(() => {
+            toast.success("Reservation cancelled");
+            router.refresh();
+        })
+        .catch(() => {
+            toast.error('Something went wrong');
+        })
+        .finally(() => {
+            setDeletingId('')
+        })
+    }, [router])
+
     return(
         <Container>
             <Heading
                 title="Reservations"
                 subtitle="Booking on your properties"
             />
+            <div
+                className="
+                    mt=10
+                    grid
+                    grid-cols-1
+                    md:grid-cols-2
+                    lg:grid-cols-3
+                    xl:grid-cols-5
+                    2xl:grid-cols-6
+                    gap-8">
+                        {reservations.map((reservation) =>(
+                            <ListingCard
+                                key={reservation.id}
+                                data={reservation.listing}
+                                reservation={reservation}
+                                actionId={reservation.id}
+                                onAction={onCancel}
+                                disabled={deletingId === reservation.id}
+                                actionLabel="Cancel guest reservation"
+                                currentUser={currentUser}/>
+                        ))}
+                    </div>
                 
         </Container>
     );

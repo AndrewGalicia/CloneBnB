445e46d3d8ba79f234bb4974f6ac025f3dca3476
Andrew Galicia <andrewlgalicia@gmail.com>
2024-06-06 17:49:50 -0700

    fixed error 3

--- app/components/Container/Container.tsx
+++ app/components/Container/Container.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react'
 
-export default function Container({children}) {
+export default function Container({children}:any) {
     return (
         <div
             className='

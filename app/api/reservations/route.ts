925ec1c2e86d500a2bdce00aa3a03cce25afdb32
Andrew Galicia <andrewlgalicia@gmail.com>
2024-06-06 17:44:44 -0700

    reserv route error

--- app/api/reservations/route.ts
+++ app/api/reservations/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import prisma from "@/app/libs/prismadb";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 
-export async function POST(request) {
+export async function POST(request: any) {
     try {
         const currentUser = await getCurrentUser();
         if (!currentUser) {
